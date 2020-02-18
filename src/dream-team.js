0860799c18d2366c750e78fe21a55e70f42641ab
Vladimir Bernacky <vladimir.bernacky@neotech.by>
2020-02-18 14:34:32 +0300

    feat add dream team script

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,16 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members ) {
+  let name=[];
+  if (Array.isArray(members)){
+   members.forEach(element => {
+     if (typeof element == 'string'){
+        name.push(element.replace(/ {1,}/g, '').charAt(0).toUpperCase());
+    } else {
+      return false;
+    }
+   });
+   
+   return name.sort().join('');
+   }else{
+     return false;
+   }
 };
