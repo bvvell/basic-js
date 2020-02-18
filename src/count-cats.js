ab77f636e42bc1a7527f65584cbccc51b2c1303d
Vladimir Bernacky <vladimir.bernacky@neotech.by>
2020-02-18 17:11:30 +0300

    fix: fix count cats function

--- src/count-cats.js
+++ src/count-cats.js
@@ -2,7 +2,7 @@ module.exports = function countCats(matrix) {
   let count = 0;
   matrix.forEach(element => {
     if (Array.isArray(element[0])) {
-      countCats[element];
+      countCats(element);
     } else {
       let a = element.filter(item => item == "^^").length;
       count += a;
