1b2b3def05ac9be61fe84e3d8c268db69513578a
Vladimir Bernacky <vladimir.bernacky@neotech.by>
2020-02-27 11:34:37 +0300

    feat: add solution for chain maker

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,24 +1,43 @@
 const chainMaker = {
+  chain: [],
+
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.chain.length();
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    // this.chain.push(String(value));
+    this.chain.push('( ' + String(value) + ' )');
+    return this;
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (this.chain.length < position || position - 1 < 0 || typeof position !== 'number') {
+      this.chain = [];
+      throw new Error('error');
+    } else {
+      this.chain.splice(position - 1, 1);
+    }
+    return this;
   },
+
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain = this.chain.reverse();
+    return this;
   },
+
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    // let result = '';
+    // for (a = 0; a <= this.chain.length - 1; a++) {
+    //   if (a === 0) {
+    //     result += '( ' + this.chain[a] + ' )'
+    //   } else {
+    //     result += '~~( ' + this.chain[a] + ' )'
+    //   }
+    // }
+    let result = this.chain.join('~~');
+    this.chain = [];
+    return result;
   }
+
 };
 
 module.exports = chainMaker;
